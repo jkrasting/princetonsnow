40410a56ab08c1d0782196703bf37a857d4651cd
John Krasting <John.Krasting@noaa.gov>
2021-12-27 19:16:32 -0500

    Calculate days since 1-Oct

--- scripts/injest.py
+++ scripts/injest.py
@@ -2,10 +2,29 @@
 
 """ Utility to work with JSON formatted snow observations """
 
+import datetime
 import json
 import os
 import re
 import sys
+
+
+def days_since_oct1(date_string):
+    """Calculates the number of days since 1-Oct"""
+    event_date = tuple([int(x) for x in date_string.split("-")])
+    event_date = datetime.datetime(*event_date)
+    ref_year = (
+        event_date.year if event_date.month in [10, 11, 12] else event_date.year - 1
+    )
+    ref_date = datetime.datetime(ref_year, 10, 1)
+    return (event_date - ref_date).days
+
+
+def fix_missing_season_date(event):
+    """Adds days since Oct 1 to older json files"""
+    if "day_of_season" not in event.keys():
+        event["day_of_season"] = days_since_oct1(event["start_date"])
+    return event
 
 
 def load_json(fname):
@@ -26,11 +45,17 @@ def create_event():
     result = {}
 
     # event start date
-    start_date = input("Start date of event (YYYY-MM-DD): ")
-    if start_date != "":
+    dates = input("Start date of event (YYYY-MM-DD): ")
+
+    if dates != "":
+        dates = dates.split(",")
+        start_date = dates[0]
+
+        end_date = dates[1] if len(dates) == 2 else ""
+
+        result["day_of_season"] = days_since_oct1(start_date)
         result["start_date"] = start_date
 
-        end_date = input("Optional -- end date of event (YYYY-MM-DD): ")
         if end_date != "":
             result["end_date"] = end_date
 
@@ -80,7 +105,7 @@ def summarize_events(event_list):
         else:
             mean = 0.0
         total += mean
-        print(f" * {_event['start_date']} - {mean}")
+        print(f" * {_event['start_date']} ({_event['day_of_season']}) - {mean}")
     total = round(total, 1)
     print("------------------")
     print(f"Total: {total}")
@@ -92,6 +117,8 @@ if __name__ == "__main__":
     if not isinstance(events, list):
         events = [events]
     numevents = len(events)
+
+    events = [fix_missing_season_date(x) for x in events]
 
     event = {}
     while event is not None:
